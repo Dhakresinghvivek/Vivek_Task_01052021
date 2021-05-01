3a5064d807453690769b21f514b490fea8703085
Vivek Singh <Vivek.Singh1@wu.com>
2021-05-01 18:54:22 +0530

    Commit_01052021

---
+++ bowling-ball/BowlingBall.Tests/GameFixture.cs
@@ -0,0 +1,124 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace BowlingBall.Tests
+{
+    [TestClass]
+    public class GameFixture
+    {
+         /*Tavisca Starter Kits 
+          Vivek Singh : Date 1st MAY 2021 
+         */
+
+
+        [TestMethod]
+        public void Gutter_game_score_should_be_zero_test()
+        {
+            var game = new Game();
+            Roll(game, 0, 20);
+            Assert.AreEqual(0, game.GetScore());
+        }
+
+        private void Roll(Game game, int pins, int times)
+        {
+            for (int i = 0; i < times; i++)
+            {
+                game.Roll(pins);
+            }
+        }
+
+        //
+        [TestMethod]
+        public void ZeroScore()
+        {
+            Game g = new Game();
+            for (int i = 0; i < 20; i++)
+                g.Roll(0);
+            Assert.AreEqual(0, g.GetScore());
+        }
+
+
+        [TestMethod]
+        public void CorrectSpareScore()
+        {
+            Game g = new Game();
+            g.Roll(5);
+            g.Roll(5);
+            g.Roll(3);
+            Assert.AreEqual(13, g.GetScore());
+        }
+
+        [TestMethod]
+        public void CorrectStrikeScore()
+        {
+            Game g = new Game();
+            g.Roll(10);
+            g.Roll(5);
+            g.Roll(6);
+            Assert.AreEqual(21, g.GetScore());
+
+        }
+
+        [TestMethod]
+        public void AllStrikes()
+        {
+            Game g = new Game();
+            for (int i = 0; i < 10; i++)
+            {
+                g.Roll(10);
+
+            }
+            Assert.AreEqual(300, g.GetScore());
+        }
+
+        [TestMethod]
+        public void AllOnes()
+        {
+            Game g = new Game();
+            for (int i = 0; i < 20; i++)
+            {
+                g.Roll(1);
+
+            }
+            Assert.AreEqual(20, g.GetScore());
+        }
+
+        [TestMethod]
+        public void AllNormals()
+        {
+            Game g = new Game();
+            //Frame 1 
+            g.Roll(3);
+            g.Roll(3);
+            //Frame 2
+            g.Roll(3);
+            g.Roll(3);
+            //Frame 3
+            g.Roll(4);
+            g.Roll(4);
+            //Frame 4 
+            g.Roll(4);
+            g.Roll(4);
+            //Frame 5 
+            g.Roll(5);
+            g.Roll(5);
+            //Frame 6 
+            g.Roll(5);
+            g.Roll(5);
+            //Frame 7 
+            g.Roll(6);
+            g.Roll(6);
+            //Frame 8 
+            g.Roll(6);
+            g.Roll(6);
+            //Frame 9  
+            g.Roll(7);
+            g.Roll(7);
+            //Frame 10 
+            g.Roll(7);
+            g.Roll(7);
+            
+            Assert.AreEqual(111, g.GetScore());
+        }
+    }
+}
---
+++ bowling-ball/BowlingBall/Game.cs
@@ -0,0 +1,121 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BowlingBall
+{
+    public class Game
+    {
+        /*Tavisca Starter Kits 
+          Vivek Singh : Date 01/05/2021  
+          VS IDE : 2019 
+          Verson : .Net Framework 4.6.1
+         * 1. Game consists of 10 frames. In each frame player will get 2 rolls to drop 10 pins.
+         * 2. If player drops all 10 pins in 1st roll,it is said to be strike and he/she will get next 2 roll's points as bonus.
+         * 3. If player drops all 10 pins in 2nd roll then he/she will get next roll's points as bonus.
+         * 4. If player drops all 10 pins in 2 rolls of a frame,it is said to be spare and he/she will get next frame's first roll's points as bonus.
+         * 5.Max score can be 300 with strike on every roll
+         * 6.Min score can be 0,no pin dropped at any roll.
+         */
+        private int _score = 0;
+        private List<int> _rolls = new List<int>();
+
+
+        public void Roll(int pins)
+        {
+            // Add your logic here. Add classes as needed.
+            _rolls.Add(pins);
+        }
+
+        public int GetScore()
+        {
+            // Returns the final score of the game.
+            int FrameIndex = 0;
+            for (int Frame = 0; Frame < 10; Frame++)
+            {
+                if (_rolls.Count - 2 == FrameIndex && _rolls[FrameIndex - 1] == 10 && _rolls[FrameIndex] != 10)
+                    break;
+                else if (FrameIndex % 2 == 0 && _rolls.Count - 1 == FrameIndex && _rolls[FrameIndex] == 10)
+                {
+                    _score = _score + _rolls[FrameIndex];
+                    Roll(10);
+                    _score += _rolls[_rolls.Count - 1];
+                    Roll(10);
+                    _score += _rolls[_rolls.Count - 1];
+                    break;
+                }
+                else if (FrameIndex % 2 == 0 && _rolls.Count - 2 == FrameIndex && _rolls[FrameIndex] == 10)
+                {
+                    Roll(10);
+                    Roll(10);
+                }
+                else if (FrameIndex % 2 == 0 && _rolls.Count - 1 == FrameIndex && _rolls[FrameIndex - 1] == 10)
+                {
+                    _score = _score + _rolls[FrameIndex];
+                    Roll(1);
+                    _score += _rolls[_rolls.Count - 1];
+                    break;
+                }
+                else if (FrameIndex % 2 != 0 && _rolls.Count - 1 == FrameIndex && _rolls[FrameIndex - 1] == 10)
+                {
+                    _score += _score + _rolls[FrameIndex];
+                    Roll(1);
+                    _score += _rolls[_rolls.Count - 1];
+                    break;
+                }
+                else if (FrameIndex + 1 == _rolls.Count || FrameIndex == _rolls.Count)
+                    break;
+
+                //If strike then get next 2 roll's points as bonus...
+                if (IsStrike(FrameIndex))
+                {
+                    _score += StrikeScore(FrameIndex);
+                    FrameIndex++;
+                }
+                else
+                {
+                    //If Spare then get next frame's first roll's points as bonus...
+                    if (IsSpare(FrameIndex))
+                    {
+                        _score += SpareScore(FrameIndex);
+                    }
+                    else
+                    {
+                        _score += FrameScore(FrameIndex);
+                    }
+                    FrameIndex += 2;
+                }
+            }
+            return _score;
+        }
+
+        //Check whether Strike...
+        private bool IsStrike(int frameIndex)
+        {
+            return _rolls[frameIndex] == 10;
+        }
+        //Calculate Frame score...
+        private int FrameScore(int frameIndex)
+        {
+            return _rolls[frameIndex] + _rolls[frameIndex + 1];
+        }
+        //If strike then get next 2 roll's points as bonus...
+        private int StrikeScore(int frameIndex)
+        {
+            return 10 + _rolls[frameIndex + 1] + _rolls[frameIndex + 2];
+        }
+        //If Spare then get next frame's first roll's points as bonus...
+        private int SpareScore(int frameIndex)
+        {
+            return 10 + _rolls[frameIndex + 2];
+        }
+        //Check whether Spare...
+        private bool IsSpare(int frameIndex)
+        {
+            return (_rolls[frameIndex] + _rolls[frameIndex + 1]) == 10;
+        }
+
+    }
+}
